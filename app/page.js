91be4e74abf103b1014b4abdb79f1701f73b15a3
Mark Baker <mrbaker1917@gmail.com>
2025-06-11 07:47:16 -0700

    updates to 2025-06-11

--- app/page.js
+++ app/page.js
@@ -1,14 +1,20 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFilm } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <header className={styles.header}>
         <h1>Star Cinema</h1>
-        <h2>Welcome to the Star Cinema in Sidney by the Sea (BC)</h2>
+        <h2>Welcome to the Star Cinema!</h2> 
+        <p>(in Sidney by the Sea)</p>
       </header>
       <main className={styles.main}>
+        <p>here are some movies</p>
+        <FontAwesomeIcon icon={faFilm} />
       </main>
       <footer></footer>
     </div>
